08e6fd2c75026668619146f2de019c9732691056
Samuel Pitoňák <sampittko@gmail.com>
2021-01-12 15:07:58 +0100

    Complete form testing

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -1,27 +1,44 @@
-// form testing
-// http://localhost:3000/login
-
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+// import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
+
+// const buildLoginForm = overrides => ({
+//   username: faker.internet.userName(),
+//   password: faker.internet.password(),
+//   ...overrides,
+// })
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  // const stubbedCredentials = {
+  //   username: 'sampittko',
+  //   password: 'myturbopassword',
+  // }
+  const {username, password} = buildLoginForm()
+
+  const handleSubmit = jest.fn()
+
+  render(<Login onSubmit={handleSubmit} />)
+
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
+
+  // userEvent.type(username, stubbedCredentials.username)
+  // userEvent.type(password, stubbedCredentials.password)
+  userEvent.type(usernameInput, username)
+  userEvent.type(passwordInput, password)
+  userEvent.click(screen.queryByRole('button', {name: /submit/i}))
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
 /*
